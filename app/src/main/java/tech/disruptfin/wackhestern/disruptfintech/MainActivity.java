2a6ee14ddc1ed8b977ccdf476b6fcb06631c3e65
austinkranc <austinkranc@rogers.com>
2017-11-18 13:17:44 -0500

    Spinner to dynamically update

--- app/src/main/java/tech/disruptfin/wackhestern/disruptfintech/MainActivity.java
+++ app/src/main/java/tech/disruptfin/wackhestern/disruptfintech/MainActivity.java
@@ -1,13 +1,42 @@
 package tech.disruptfin.wackhestern.disruptfintech;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends Activity {
+    Spinner spinner;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        spinner = (Spinner) findViewById(R.id.spinner1);
+        ArrayAdapter<String> adapter;
+        ArrayList<String> list;
+
+        list = new ArrayList<String>();
+        for(String string:getResources().getStringArray(R.array.country_arrays)){
+            list.add(string);
+        }
+        adapter = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_spinner_item, list);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        //adapter.add(list.get(0));
+        spinner.setAdapter(adapter);
+        adapter.notifyDataSetChanged();
+    }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+      /**/
     }
 }
